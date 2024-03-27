8e2f49ff29226cd87942d6d9ec5992582043b014
hakankulak <hakankulak1@outlook.com>
2024-03-27 21:43:15 +0300

    init: fundamental functions integrated

---
+++ fundamentals.r
@@ -0,0 +1,17 @@
+#Create data
+data <- data.frame(
+  name=c( "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"),
+  value=c( 3, 12, 5, 18, 45, 6, 7, 8, 9, 10)
+)
+#Calculate Average
+average <- mean(data$value)
+
+#Create a new column with the average
+data$average <- average
+
+#Create a new column with the difference
+data$difference <- data$value - average
+
+#Print the data
+print(data)
+ 
